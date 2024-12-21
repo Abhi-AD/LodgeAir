192bc82d368b874d1fb000ace024b844e5578f87
Abhi-AD <dangiabhi332@gmail.com>
2024-12-21 22:23:10 +0545

    Bakend: add the make it possible connect the chat

--- backend/apps/chat/urls.py
+++ backend/apps/chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.chat.api import conversations_list
+from apps.chat.api import conversations_list, conversations_detail
 
 
 urlpatterns = [
     path("", conversations_list, name="conversations_list"),
+    path("<uuid:pk>/", conversations_detail, name="conversations_detail"),
 ]
